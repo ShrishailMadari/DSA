2790dfccc96e19ee581814d6a21c004982ca5e7d
Shrishail <shrishailmadari@gmail.com>
2024-10-13 00:55:41 +0530

    add these java files

--- practiceproblems/TracingArrays.java
+++ practiceproblems/TracingArrays.java
@@ -2,9 +2,14 @@ package practiceproblems;
 
 public class TracingArrays {
     public static void main(String[] args) {
-
+    traceArray();
     }
     public static void traceArray(){
-        
+        int a[] ={1,2,3,4,5,6,7};
+        System.out.println(a.length-1);
+
+        for (int i = 0; i<=a.length-1; i++){
+            System.out.println(a[i]);
+        }
     }
 }
